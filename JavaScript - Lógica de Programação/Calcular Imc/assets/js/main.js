99c024cea9989b3ce0a7c50bbb22c6933083f04e
Jacob Alexandre <49577844+JacobAraujo@users.noreply.github.com>
2022-04-07 05:38:48 -0300

    Progresso do curso
    Criado uma pasta especificamente para o capítulo do curso e progredindo.

---
+++ JavaScript - Lógica de Programação/Calcular Imc/assets/js/main.js
@@ -0,0 +1,9 @@
+function meuEscopo(){
+    const form = document.querySelector(".form")
+    
+    function recebeEventoForm(evento){
+        evento.preventDefault()
+
+        let peso = document.querySelector('')
+    }
+}
